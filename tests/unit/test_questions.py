8c1fb369c9f728eb5fd29979824d196eec142da8
psarma89 <priyam.sarma@gmail.com>
2018-06-16 22:43:24 -0400

    Add unit tests

--- tests/unit/test_questions.py
+++ tests/unit/test_questions.py
@@ -1,10 +1,10 @@
 import unittest
-import spotify.questions
+from questions import question1, question2, question3
 
 class TestQuestions(unittest.TestCase):
 
     def question1(self):
-        self.assertEqual(questions.question1.sort_by_string("weather", "therapyw"), "theeraw")
+        self.assertEqual(question1.sort_by_string("weather", "therapyw"), "theeraw")
         self.assertEqual(question1.sort_by_string("good", "odg"), "oodg")
 
     def question2(self):
